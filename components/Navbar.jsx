d7412fe425758559c314d72b6aa9671657af6b4f
a-n-a-s <mirzaanas815@gmail.com>
2022-05-21 22:24:34 +0530

    Day 1

---
+++ components/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
