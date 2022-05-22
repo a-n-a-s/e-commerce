618a92235fa66bc108b7f4eb01f9c57be7e62a8e
a-n-a-s <mirzaanas815@gmail.com>
2022-05-22 11:21:34 +0530

    2:00:00

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import Link from "next/link";
 import { AiOutlineShopping } from "react-icons/ai";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">OSM Headphones</Link>
+      </p>
+      <button type="button" onClick="" className="cart-icon">
+        <AiOutlineShopping />
+        <span className="cart-item-qty">1</span>
+      </button>
+    </div>
+  );
 };
 
 export default Navbar;
